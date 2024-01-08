9f105c8761fd106b578ad11e502546e85290026d
Percy <percy@railway.app>
2024-01-08 22:35:47 +0000

    Initial Commit

---
+++ server/commands/🔰Group/mention.js
@@ -0,0 +1,112 @@
+"◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱( Ⓒ𝐁𝐥𝐨𝐨𝐦𝐁𝐨𝐭 (𝐦𝐮𝐥𝐭𝐢-𝐝𝐞𝐯𝐢𝐜𝐞) 𝐛𝐲 𝐌𝐚𝐠𝐧𝐞𝐮𝐦™ )☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎";
+//  ╔⧉༻ Ⓒ𝐁𝐥𝐨𝐨𝐦𝐁𝐨𝐭 (𝐦𝐮𝐥𝐭𝐢-𝐝𝐞𝐯𝐢𝐜𝐞) 𝐛𝐲 𝐌𝐚𝐠𝐧𝐞𝐮𝐦™
+//  ║>>  is a whatsapp user-bot with automation, moderation, music, games and more of 200+ commands!
+//  ║
+//  ║
+//  ║⦁> 🌟 A versatile whatsApp multi-purpose bot designed for group management and user convenience.
+//  ║⦁> 🚀 Simplifies group management tasks and enhances the overall user experience.
+//  ║⦁> ⚠️ Please note: Engaging in spamming activities may lead to account suspension. Use responsibly!
+//  ║⦁> 🎉 BloomBot is intended for fun and convenience, but we're not responsible for account bans.
+//  ║⦁> 🔀 forking the repository is allowed, but customized versions or modified plugins are unsupported.
+//  ║⦁> ⚠️ Exercise caution and take responsibility for any modifications made to the bot.
+//  ║⦁> 📞 Need assistance or have issues? Contact our developers.
+//  ║⦁> 🔄 We'll continue providing updates and support for the original version of the bot.
+//  ║⦁> 👉 Enjoy the features and functionality of BloomBot responsibly! Make the most out of your
+//  ║    whatsApp group management experience! 🎉
+//  ║
+//  ║     🚨𝐔𝐬𝐚𝐠𝐞 𝐍𝐨𝐭𝐢𝐜𝐞🚨
+//  ║⦁>    ⒸBloomBot is in no way affiliated with, authorized, maintained,
+//  ║⦁>    sponsored or endorsed by whatsApp or any of its affiliates or
+//  ║⦁>    subsidiaries. This is an independent and unofficial software.
+//  ║⦁>    Use at your own risk.
+//  ║
+//  ╚◎ ⚙️Developers: +918436686758, +918250889325
+"◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱( Ⓒ𝐁𝐥𝐨𝐨𝐦𝐁𝐨𝐭 (𝐦𝐮𝐥𝐭𝐢-𝐝𝐞𝐯𝐢𝐜𝐞) 𝐛𝐲 𝐌𝐚𝐠𝐧𝐞𝐮𝐦™ )☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎";
+require("#/config/index.js");
+const ppth = require("path");
+const tpth = ppth.basename(__filename);
+const currFile = tpth.slice(0, -3).toLowerCase();
+
+module.exports = async (
+  BloomBot,
+  chatkey,
+  gmeta,
+  isAdmin,
+  groupName,
+  isbotAdmin,
+  groupAdmins,
+  participants
+) => {
+  try {
+    switch (true) {
+      case !chatkey.isGroup:
+        await BloomBot.sendMessage(chatkey.chat, {
+          react: {
+            text: "❌",
+            key: chatkey.key,
+          },
+        });
+        return chatkey.reply(
+          `*😥Apologies:* _${BloomBot.pushname || BloomBot.tagname}_
+
+*❌Error:*  
+> _This command can only be used in groups!_`
+        );
+
+      case !(isAdmin || BloomBot.isSudo):
+        await BloomBot.sendMessage(chatkey.chat, {
+          react: {
+            text: "❌",
+            key: chatkey.key,
+          },
+        });
+        return chatkey.reply(
+          `*😥Apologies:* _${BloomBot.pushname || BloomBot.tagname}_
+
+*❌Error:* 
+> _This is an admin/sudo-only command!_`
+        );
+
+      default:
+        let imåge = "./server/public/mention.png";
+        let buffer = Buffer.isBuffer(imåge)
+          ? imåge
+          : /^data:.*?\/.*?;base64,/i.test(imåge)
+          ? Buffer.from(imåge.split(",")[1], "base64")
+          : /^https?:\/\//.test(imåge)
+          ? await BloomBot.getBuffer(imåge)
+          : BloomBot.fs.existsSync(imåge)
+          ? BloomBot.fs.readFileSync(imåge)
+          : Buffer.alloc(0);
+
+        let message = BloomBot.args
+          ? BloomBot.args.join(" ")
+          : "Attention Everyone";
+        let caption = `*📢 Chat Id:* ${chatkey.chat}
+*💫 Pinged By:* ${BloomBot.pushname || "ɴᴏ_ɴᴀᴍᴇ"}
+*🕛 Time:* ${BloomBot.moment.tz("Asia/Kolkata").format("DD/MM HH:mm:ss")}
+*📌 Message:* \n${message}`;
+
+        await BloomBot.sendMessage(chatkey.chat, {
+          image: buffer,
+          caption,
+          mentions: await participants.map((a) => a.id),
+        }).catch((e) => console.log(e));
+    }
+  } catch (error) {
+    return BloomBot.handlerror(BloomBot, chatkey, error);
+  }
+};
+
+module.exports.aliases = [
+  "tagalls",
+  "callall",
+  "alltag",
+  "allping",
+  "groupall",
+  "notifall",
+  "massping",
+  "crowdtag",
+  "teamall",
+  "groupmsg",
+];
